Fixed perplexity calculation

DataSet.py
@@ -1,6 +1,5 @@
 import os
 import torch
-from torch.utils.data import Dataset
 from torch.autograd import Variable
 
 
@@ -23,13 +22,14 @@ class Dictionary(object):
 
 class DataLoader(object):
     """Corpus Tokenizer"""
-    def __init__(self, data_path, batch_size, sequence_length):
-        self.batch_size = batch_size
+    def __init__(self, data_path, train_batch_size, eval_batch_size, sequence_length):
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
         self.sequence_length = sequence_length
         self.dictionary = Dictionary()
-        self.train = self.batchify(self.tokenize(os.path.join(data_path, 'ptb.train.txt')))
-        self.valid = self.batchify(self.tokenize(os.path.join(data_path, 'ptb.valid.txt')))
-        self.test = self.batchify(self.tokenize(os.path.join(data_path, 'ptb.test.txt')))
+        self.train = self.batchify(self.tokenize(os.path.join(data_path, 'ptb.train.txt')), self.train_batch_size)
+        self.valid = self.batchify(self.tokenize(os.path.join(data_path, 'ptb.valid.txt')), self.eval_batch_size)
+        self.test = self.batchify(self.tokenize(os.path.join(data_path, 'ptb.test.txt')), self.eval_batch_size)
 
     def tokenize(self, path):
         """Tokenizes a text file."""
@@ -56,22 +56,26 @@ class DataLoader(object):
 
         return ids
 
-    def batchify(self, data):
-        number_of_batches = data.size(0) // self.batch_size
-        data = data.narrow(0, 0, number_of_batches * self.batch_size)
-        data = data.view(self.batch_size, -1).t().contiguous()
+    def batchify(self, data, batch_size):
+        number_of_batches = data.size(0) // batch_size
+        data = data.narrow(0, 0, number_of_batches * batch_size)
+        data = data.view(batch_size, -1).t().contiguous()
         return data
 
-    def get_batch(self, source, i, evaluation=False):
+    def get_batch(self, source, i):
         seq_len = min(self.sequence_length, len(source) - 1 - i)
-        data = Variable(source[i:i + seq_len], volatile=evaluation)
+        data = Variable(source[i:i + seq_len])
         target = Variable(source[i + 1:i + 1 + seq_len].view(-1))
         return data, target
 
 
 if __name__ == "__main__":
     data_path = "./data"
-    batch_size = 20
+    train_batch_size = 20
+    eval_batch_size = 20
+    sequence_length = 35
     dataset = DataLoader(data_path=data_path,
-                         batch_size=batch_size)
+                         train_batch_size=train_batch_size,
+                         eval_batch_size=eval_batch_size,
+                         sequence_length=sequence_length)
 

Handler.py
@@ -1,62 +1,233 @@
+import time
+
+import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from DataSet import DataSet
+from DataSet import DataLoader
 from model import LSTM_Model, GRU_Model
+import numpy as np
+import os
+import math
+
+
+torch.manual_seed(1111)
 
 
 class Handler:
-    def __init__(self, model, model_name, data_path, optimizer, epochs, lr, batch_size, device):
+    def __init__(self, model, model_name, data_path, train_batch_size, eval_batch_size, sequence_length, criterion,
+                 epochs, lr, dropout, device):
         self.model = model
         self.model_name = model_name
         self.data_path = data_path
-        self.optimizer = optimizer
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
+        self.sequence_length = sequence_length
+        self.criterion = criterion
         self.epochs = epochs
         self.lr = lr
-        self.batch_size = batch_size
+        self.lr_tracker = []
+        self.dropout = dropout
         self.device = device
-        self.train_dataset = DataSet(path=data_path)
+        self.dataloader = DataLoader(data_path=data_path, train_batch_size=train_batch_size,
+                                     eval_batch_size=eval_batch_size, sequence_length=sequence_length)
 
         self.model.to(device)
 
     def train_one_epoch(self, epoch):
         self.model.train()
 
-        train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        train_dataloader = self.dataloader.train
 
-        print_every = len(train_dataloader) // 10
-        hidden = self.model.init_hidden(self.batch_size)
-        for batch_idx, sentence in enumerate(train_dataloader):
-            sentence = sentence.to(self.device)
-            hidden = hidden.to(device)
+        total_loss = 0
+        avg_loss = 0
+        avg_perplexity = 0
+        print_every = 200
 
-            result, hidden = self.model(sentence, hidden)
+        hidden = self.model.init_hidden(self.train_batch_size, device)
+        for batch_idx, i in enumerate(range(0, train_dataloader.size(0) - 1, self.sequence_length)):
+            data, target = self.dataloader.get_batch(train_dataloader, i)
+
+            data = data.to(self.device)
+            target = target.to(self.device)
+
+            hidden = self.model.detach_hidden(hidden, device)
+
+            self.model.zero_grad()
+
+            result, hidden = self.model(data, hidden)
+            loss = self.criterion(result, target)
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.25)
+            for p in self.model.parameters():
+                p.data.add_(p.grad.data, alpha=-self.lr)
+
+            total_loss += loss.item()
+            avg_loss = total_loss / (batch_idx + 1)
+            avg_perplexity = math.exp(avg_loss)
+
+            if batch_idx % print_every == 0 and batch_idx > 0:
+                print(f'Epoch[{epoch + 1:03d}] | {batch_idx} / {len(train_dataloader)// self.sequence_length} '
+                      f'batches | Loss: {avg_loss:.2f} | lr {self.lr:.2f} | Perplexity: {avg_perplexity:.2f}')
+
+        return avg_loss, avg_perplexity
+
+    def evaluate_model(self, dataloader, epoch, data_type):
+        self.model.eval()
+
+        total_loss = 0
+        avg_loss = 0
+        if data_type == "Train":
+            hidden = self.model.init_hidden(self.train_batch_size, device)
+        else:
+            hidden = self.model.init_hidden(self.eval_batch_size, device)
+        for batch_idx, i in enumerate(range(0, dataloader.size(0) - 1, self.sequence_length)):
+            with torch.no_grad():
+                data, target = self.dataloader.get_batch(dataloader, i)
+
+                data = data.to(self.device)
+                target = target.to(self.device)
+
+                result, hidden = self.model(data, hidden)
+
+                loss = self.criterion(result, target)
+
+                total_loss += loss.item()
+                avg_loss = total_loss / (batch_idx + 1)
+
+        return avg_loss, math.exp(avg_loss)
+
+    def save_data(self, average_train_perplexity, average_validation_perplexity, average_test_perplexity):
+        plt.plot(average_train_perplexity, label="Train Perplexity")
+        plt.plot(average_test_perplexity, label="Test Perplexity")
+        plt.title(f"{self.model_name}: lr {self.lr:.4f} Dropout {self.dropout}")
+        plt.xlabel("Epoch")
+        plt.ylabel("Perplexity")
+        plt.legend(loc="upper right")
+        plt.xticks(np.arange(0, len(average_train_perplexity), 5))
+        plt.grid()
+        plt.savefig(os.path.join(os.getcwd(), "saved_models", self.model_name, "Convergence Graph.png"))
+        plt.close()
+
+        plt.plot(self.lr_tracker)
+        plt.title("Learning Rate")
+        plt.xlabel("Epoch")
+        plt.ylabel("lr")
+        plt.xticks(np.arange(0, len(self.lr_tracker), 5))
+        plt.grid()
+        plt.savefig(os.path.join(os.getcwd(), "saved_models", self.model_name, "LR Graph.png"))
+        plt.close()
+
+        train_perplexity_filepath = os.path.join(os.getcwd(), "saved_models", self.model_name,
+                                                 "Train Perplexity Values.log")
+        test_perplexity_filepath = os.path.join(os.getcwd(), "saved_models", self.model_name,
+                                                "Test Perplexity Values.log")
+        validation_perplexity_filepath = os.path.join(os.getcwd(), "saved_models", self.model_name,
+                                                      "Validation Perplexity Values.log")
+        train_file = open(train_perplexity_filepath, "w")
+        test_file = open(test_perplexity_filepath, "w")
+        validation_file = open(validation_perplexity_filepath, "w")
+
+        for i in range(len(average_train_perplexity)):
+            if i != len(average_train_perplexity) - 1:
+                train_file.write(str(average_train_perplexity[i]) + ", ")
+                test_file.write(str(average_test_perplexity[i]) + ", ")
+                validation_file.write(str(average_validation_perplexity[i]) + ", ")
+            else:
+                train_file.write(str(average_train_perplexity[i]))
+                test_file.write(str(average_test_perplexity[i]))
+                validation_file.write(str(average_validation_perplexity[i]))
+        train_file.write(f'\nLowest Perplexity: {min(average_train_perplexity)}')
+        test_file.write(f'\nLowest Perplexity: {min(average_test_perplexity)}')
+        validation_file.write(f'\nLowest Perplexity: {min(average_validation_perplexity)}')
+
+        train_file.close()
+        test_file.close()
+        validation_file.close()
+
+    def run(self):
+        print(f'Start Running {self.model_name}')
+        if not os.path.exists(os.path.join(os.getcwd(), "saved_models")):
+            os.mkdir(os.path.join(os.getcwd(), "saved_models"))
+        if not os.path.exists(os.path.join(os.getcwd(), "saved_models", self.model_name)):
+            os.mkdir(os.path.join(os.getcwd(), "saved_models", self.model_name))
+
+        best_loss = torch.inf
+        checkpoint_filename = os.path.join(os.getcwd(), "saved_models", self.model_name, f'{self.model_name}.pt')
+
+        train_perplexity = []
+        test_perplexity = []
+        validation_perplexity = []
+        for epoch in range(self.epochs):
+            start = time.time()
+            self.lr_tracker.append(self.lr)
+            print("-" * 80)
+            self.train_one_epoch(epoch)
+
+            train_avg_loss, train_avg_perplexity = self.evaluate_model(self.dataloader.train, epoch, "Train")
+            validation_avg_loss, validation_avg_perplexity = self.evaluate_model(self.dataloader.valid, epoch, "Validation")
+            test_avg_loss, test_avg_perplexity = self.evaluate_model(self.dataloader.test, epoch, "Test")
+            print("-" * 80)
+            end = time.time()
+            print(f"| End of epoch {epoch + 1} | Epoch RunTime {(end - start):.2f}s | Train ppl "
+                  f"{train_avg_perplexity:.2f} | Valid ppl {validation_avg_perplexity:.2f} | Test ppl "
+                  f"{test_avg_perplexity:.2f}")
+
+            train_perplexity.append(train_avg_perplexity)
+            test_perplexity.append(test_avg_perplexity)
+            validation_perplexity.append(validation_avg_perplexity)
+
+            if validation_avg_loss < best_loss:
+                print(f"Saving Checkpoint {checkpoint_filename}")
+                state = {
+                    "model": self.model.state_dict()
+                }
+                torch.save(state, checkpoint_filename)
+                best_loss = validation_avg_loss
+            else:
+                self.lr /= 4.0
+
+        self.save_data(train_perplexity, validation_perplexity, test_perplexity)
 
 
 if __name__ == "__main__":
-    dropout = 0.5
+    dropout = 0
     input_size = 1
     hidden_size = 200
     num_layers = 2
-    model = LSTM_Model(input_size=input_size,
-                       hidden_size=hidden_size,
-                       num_layers=num_layers,
-                       dropout=dropout)
-    model_name = "LSTM_Dropout"
-    data_path = "./data/ptb.train.txt"
-    lr = 0.001
-    optimizer = optim.AdamW(model.parameters(), lr=lr)
-    epochs = 10
-    batch_size = 20
+    num_tokens = 10000
+    num_embeddings = 200
+    sequence_length = 35
+    criterion = nn.CrossEntropyLoss()
+    epochs = 40
+    train_batch_size = 20
+    eval_batch_size = 10
+    lr = 20
+    lstm_model = LSTM_Model(hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            dropout=dropout,
+                            num_tokens=num_tokens,
+                            num_embeddings=num_embeddings)
+    # gru_model = GRU_Model(hidden_size=hidden_size,
+    #                       num_layers=num_layers,
+    #                       dropout=dropout,
+    #                       num_tokens=num_tokens,
+    #                       num_embeddings=num_embeddings)
+    model_name = "LSTM_Dropout_0"
+    data_path = "./data"
+
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    handler = Handler(model=model,
+    handler = Handler(model=lstm_model,
                       model_name=model_name,
                       data_path=data_path,
-                      optimizer=optimizer,
+                      train_batch_size=train_batch_size,
+                      eval_batch_size=eval_batch_size,
+                      sequence_length=sequence_length,
+                      criterion=criterion,
                       epochs=epochs,
                       lr=lr,
-                      batch_size=batch_size,
-                      device=device
-                      )
-    handler.train_one_epoch(0)
+                      dropout=dropout,
+                      device=device)
+    handler.run()

model.py
@@ -19,20 +19,24 @@ class LSTM_Model(nn.Module):
         output, hidden = self.lstm(x, hidden)
         output = self.drop(output)
         decoded_output = self.decoder(output.view((output.size(0) * output.size(1), output.size(2))))
-        decoded_output = decoded_output.view((output.size(0), output.size(1), decoded_output.size(1)))
         return decoded_output, hidden
 
-    def init_hidden(self, batch_size):
+    def init_hidden(self, batch_size, device):
         h = torch.zeros(self.num_layers, batch_size, self.hidden_size)
         c = torch.zeros(self.num_layers, batch_size, self.hidden_size)
-        return h, c
+        return h.to(device), c.to(device)
 
     def init_model(self):
-        for name, param in self.lstm.named_parameters():
-            if "weight" in name:
-                nn.init.uniform_(param.data, a=-0.1, b=0.1)
-            elif "bias" in name:
-                nn.init.constant_(param.data, 0.0)
+        self.encoder.weight.data.uniform_(-0.1, 0.1)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-0.1, 0.1)
+
+    def detach_hidden(self, hidden, device):
+        h, c = hidden
+        h = h.detach().to(device)
+        c = c.detach().to(device)
+        hidden = (h, c)
+        return hidden
 
 
 class GRU_Model(nn.Module):
@@ -45,25 +49,34 @@ class GRU_Model(nn.Module):
         self.encoder = nn.Embedding(num_tokens, num_embeddings)
         self.gru = nn.GRU(input_size=num_embeddings, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout)
         self.decoder = nn.Linear(hidden_size, num_tokens)
+        self.init_model()
 
     def forward(self, x, hidden):
         x = self.drop(self.encoder(x))
         output, hidden = self.gru(x, hidden)
         output = self.drop(output)
         decoded_output = self.decoder(output.view(output.size(0) * output.size(1), output.size(2)))
-        decoded_output = decoded_output.view((output.size(0), output.size(1), decoded_output.size(1)))
         return decoded_output, hidden
 
-    def init_hidden(self, batch_size):
+    def init_hidden(self, batch_size, device):
         h = torch.zeros(self.num_layers, batch_size, self.hidden_size)
-        return h
+        return h.to(device)
+
+    def init_model(self):
+        self.encoder.weight.data.uniform_(-0.1, 0.1)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-0.1, 0.1)
+
+    def detach_hidden(self, hidden, device):
+        hidden = hidden.detach().to(device)
+        return hidden
 
 
 if __name__ == "__main__":
     input_size = 1
     hidden_size = 200
     num_layers = 2
-    dropout = 0.5
+    dropout = 0.2
     batch_size = 20
     num_tokens = 10000
     num_embeddings = 256
@@ -73,8 +86,7 @@ if __name__ == "__main__":
                             num_layers=num_layers,
                             dropout=dropout,
                             num_tokens=num_tokens,
-                            num_embeddings=num_embeddings
-                            )
+                            num_embeddings=num_embeddings)
 
     gru_model = GRU_Model(hidden_size=hidden_size,
                           num_layers=num_layers,